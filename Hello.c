c0c9532b62d184b94f7efee3d3f303ae897731f2
djcooland <larsdjand@gmail.com>
2018-12-03 13:32:44 +0100

    cleanup

--- Hello.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.c>
-
-int main()
-{
-    printf("Hat og briller");
-    return 0;
-}
--- main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.c>
-
-int main()
-{
-    printf("Hat og briller");
-    printf("Hello world");
-    return 0;
-}
